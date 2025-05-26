eb4b41b3ed1b712419e1a737cd21edbb1781f2d7
Micthelle michael <micthelle.chinwendu.michael@stu.ocsb.ca>
2025-05-26 16:57:02 +0000

    added rst-01

---
+++ rst-01.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# Created By: Micthelle Michael
+# Date: may 26, 2025
+
+print ("Hello, Michael!")
+# This program prints a greeting message to the user.
+# It is a simple demonstration of how to output text in Python.
